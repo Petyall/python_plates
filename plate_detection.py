e510f5b76d2e0dd98f93edca2c6245fbf1a08227
petyal <alekolen35@gmail.com>
2024-12-22 11:44:10 +0300

    project structure modified

--- plate_detection.py
+++ plate_detection.py
@@ -4,40 +4,41 @@ from ultralytics import YOLO
 
 
 # Загрузка натренированной модели
-model = YOLO('./trained_models/best.pt')
+model = YOLO('./trained_models/plate_detection.pt')
 
-# Путь до фотографии
-image_path = './cars/16.jpg'
+for i in range(1, 16):
+    # Путь до фотографии
+    image_path = f'./data/cars/{i}.jpg'
 
-# Поиск автомобильного номера на фотографии
-results = model.predict(source=image_path, save=False)
+    # Поиск автомобильного номера на фотографии
+    results = model.predict(source=image_path, save=False)
 
-# Загрузка оригинального изображения
-img = cv2.imread(image_path)
+    # Загрузка оригинального изображения
+    img = cv2.imread(image_path)
 
-# Параметр отступа пикселей на изображении (нужно, чтобы случайно не обрезать символы на номере)
-padding = 10
+    # Параметр отступа пикселей на изображении (нужно, чтобы случайно не обрезать символы на номере)
+    padding = 10
 
 
-# Цикл, перебирающий все распознанные номерные знаки на изображении
-for result in results:
-    # Получение координат ограничивающего прямоугольника (границы распознанного номера на изображении)
-    boxes = result.boxes.xyxy.cpu().numpy()  # Перевод координат в массив
+    # Цикл, перебирающий все распознанные номерные знаки на изображении
+    for result in results:
+        # Получение координат ограничивающего прямоугольника (границы распознанного номера на изображении)
+        boxes = result.boxes.xyxy.cpu().numpy()  # Перевод координат в массив
 
-    for box in boxes:
-        x1, y1, x2, y2 = map(int, box)  # Перевод координаты в int
+        for box in boxes:
+            x1, y1, x2, y2 = map(int, box)  # Перевод координаты в int
 
-        # Добавление отступов с условием, чтобы они не выходили за границы изображения
-        x1 = max(0, x1 - padding)
-        y1 = max(0, y1 - padding)
-        x2 = min(img.shape[1], x2 + padding)
-        y2 = min(img.shape[0], y2 + padding)
+            # Добавление отступов с условием, чтобы они не выходили за границы изображения
+            x1 = max(0, x1 - padding)
+            y1 = max(0, y1 - padding)
+            x2 = min(img.shape[1], x2 + padding)
+            y2 = min(img.shape[0], y2 + padding)
 
-        # Вырезание номерного знака с изображения
-        plate = img[y1:y2, x1:x2]
+            # Вырезание номерного знака с изображения
+            plate = img[y1:y2, x1:x2]
 
-        # Вывод автомобильного номера
-        cv2.imshow('License Plate', plate)
-        cv2.imwrite('16.jpg', plate)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+            # Вывод автомобильного номера
+            # cv2.imshow('License Plate', plate)
+            cv2.imwrite(f'./data/detected_plates/{i}.jpg', plate)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
